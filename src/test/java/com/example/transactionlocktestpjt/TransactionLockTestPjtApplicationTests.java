7b9153463a320e354104be3cbdb73e9a2fcc2a1a
seonb2n <626ksb@gmail.com>
2024-03-11 20:42:00 +0900

    project: init pjt

---
+++ src/test/java/com/example/transactionlocktestpjt/TransactionLockTestPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionlocktestpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionLockTestPjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
