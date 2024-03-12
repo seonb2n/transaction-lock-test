d93f5f0cc5cc57f7f7b3318d113121cefd469964
seonb2n <626ksb@gmail.com>
2024-03-12 21:43:32 +0900

    feature: queryDSL 추가

--- src/main/java/com/example/transactionlocktestpjt/service/PocketService.java
+++ src/main/java/com/example/transactionlocktestpjt/service/PocketService.java
@@ -1,5 +1,21 @@
 package com.example.transactionlocktestpjt.service;
 
+import com.example.transactionlocktestpjt.repository.PocketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class PocketService {
+
+    private final PocketRepository pocketRepository;
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void serializeTransaction() {
+
+    }
+
 
 }
--- src/main/java/com/example/transactionlocktestpjt/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.example.transactionlocktestpjt.service;
-
-public class UserService {
-
-}
